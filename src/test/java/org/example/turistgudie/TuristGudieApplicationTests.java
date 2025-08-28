a5ec62ae361b052ebb49be737a744d4569fafe13
SebastianMunt <semu0001@stud.kea.dk>
2025-08-28 12:35:36 +0200

    3.

---
+++ src/test/java/org/example/turistgudie/TuristGudieApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.turistgudie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TuristGudieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
